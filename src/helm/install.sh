d88066d9f0a662ab46a126ffcf774a269083c377
Geekstuff Dev <as-gsdev@geekstuff.it>
2023-11-17 20:30:57 +0000

    Update features from sources

--- src/helm/install.sh
+++ src/helm/install.sh
@@ -10,7 +10,8 @@ fi
 # helm
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
-./get_helm.sh
+DESIRED_VERSION=${VERSION} ./get_helm.sh
+
 rm ./get_helm.sh
 
 # helm completion
