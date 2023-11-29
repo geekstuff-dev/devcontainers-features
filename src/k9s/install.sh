9fff74afd7ca61d3e9fc47027a4450cc17c58535
Geekstuff Dev <as-gsdev@geekstuff.it>
2023-11-29 17:51:49 +0000

    Update features from sources

--- src/k9s/install.sh
+++ src/k9s/install.sh
@@ -11,6 +11,7 @@ TMP_DIR=$(mktemp -d)
 TARGET="$TMP_DIR/k9s.tar.gz"
 BIN="$TMP_DIR/k9s"
 
+# install if not present
 if ! command -v k9s &>/dev/null; then
     curl -fsSL -o $TARGET https://github.com/derailed/k9s/releases/download/${VERSION}/k9s_Linux_amd64.tar.gz
     tar -C $TMP_DIR -zxf $TARGET
@@ -18,7 +19,6 @@ if ! command -v k9s &>/dev/null; then
     rm -rf $TMP_DIR
 fi
 
-### k9s autocomplete
-###   disabled since it has one but does not seem to autocomplete anything
-# mkdir -p /etc/bash_completion.d
-# k9s completion bash > /etc/bash_completion.d/k9s
+# ensure autocomplete
+mkdir -p /etc/bash_completion.d
+k9s completion bash > /etc/bash_completion.d/k9s
