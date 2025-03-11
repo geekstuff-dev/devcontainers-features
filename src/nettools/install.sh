85f287b849361cfd0a56f29938b650681335e82f
Geekstuff Dev <as-gsdev@geekstuff.it>
2025-03-11 15:00:00 +0000

    Update features from sources

--- src/nettools/install.sh
+++ src/nettools/install.sh
@@ -3,6 +3,8 @@
 set -e
 
 if command -v apt; then
+    # Ideally this would be present at build time in all devcontainers
+    export DEBIAN_FRONTEND=noninteractive
     apt-get update
     apt-get install -y dnsutils netcat-openbsd
     rm -rf /var/lib/apt/lists/*
