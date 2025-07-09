fb3f5967ec2c2439cd3b5756f1392fc89b9618a2
Geekstuff Dev <as-gsdev@geekstuff.it>
2025-07-09 14:03:59 +0000

    Update features from sources

--- src/shellcheck/install.sh
+++ src/shellcheck/install.sh
@@ -18,7 +18,7 @@ BIN="$TMP_DIR/shellcheck-${VERSION}/shellcheck"
 # install if not present
 if ! command -v shellcheck &>/dev/null; then
     curl -fsSL -o $TARGET https://github.com/koalaman/shellcheck/releases/download/v0.10.0/shellcheck-${VERSION}.linux.x86_64.tar.xz
-    tar -C $TMP_DIR -zxf $TARGET
+    tar -xf $TARGET -C $TMP_DIR
     install -m a+rx $BIN /usr/local/bin
     rm -rf $TMP_DIR
 fi
