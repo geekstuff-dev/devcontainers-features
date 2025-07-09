1af6d8e32a82ebc334bbcb6c4a01e1fbb09f4d67
Geekstuff Dev <as-gsdev@geekstuff.it>
2025-07-09 14:21:20 +0000

    Update features from sources

--- src/shellcheck/install.sh
+++ src/shellcheck/install.sh
@@ -15,6 +15,20 @@ TMP_DIR=$(mktemp -d)
 TARGET="$TMP_DIR/shellcheck.tar.xz"
 BIN="$TMP_DIR/shellcheck-${VERSION}/shellcheck"
 
+# xz package necessary
+if ! command -v xz >/dev/null 2>&1; then
+    if command -v apt >/dev/null 2>&1; then
+        apt-get update
+        DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends xz-utils
+        rm -rf /var/lib/apt/lists/*
+    elif command -v apk >/dev/null 2>&1; then
+        apk add --no-cache xz
+    else
+        echo "error: unable to install xz in this OS distro"
+        exit 1
+    fi
+fi
+
 # install if not present
 if ! command -v shellcheck &>/dev/null; then
     curl -fsSL -o $TARGET https://github.com/koalaman/shellcheck/releases/download/v0.10.0/shellcheck-${VERSION}.linux.x86_64.tar.xz
