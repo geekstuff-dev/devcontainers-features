20c93811e43099e804aaa512202424cc469852f7
Geekstuff Dev <as-gsdev@geekstuff.it>
2026-05-01 18:49:09 +0000

    Update features from sources

--- src/k9s/install.sh
+++ src/k9s/install.sh
@@ -7,6 +7,22 @@ if test -e $LIB_DEVCONTAINER_FEATURES/buildtime-http-proxy.sh; then
     . $LIB_DEVCONTAINER_FEATURES/buildtime-http-proxy.sh
 fi
 
+# If K9s present and version does not match, delete it and redownload
+CURR_K9S_PATH=$(command -v k9s)
+if test -n "$CURR_K9S_PATH"; then
+    CURR_K9S_VERSION=$(k9s version -s | grep Version | rev | cut -d' ' -f1 | rev)
+    echo "Existing k9s version: $CURR_K9S_VERSION"
+    if test "$CURR_K9S_VERSION" != "$VERSION"; then
+        echo "Target version is $VERSION, delete different current version"
+        rm -f "$CURR_K9S_PATH"
+    fi
+
+    # Also delete auto-completion to get the fresh version
+    if test -e /etc/bash_completion.d/k9s; then
+        rm -f /etc/bash_completion.d/k9s
+    fi
+fi
+
 TMP_DIR=$(mktemp -d)
 TARGET="$TMP_DIR/k9s.tar.gz"
 BIN="$TMP_DIR/k9s"
